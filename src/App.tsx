45bec3f608852a49b8795bd66d0de2b262a9420b
alana bookman <alanaslc033@gmail.com>
2025-04-24 12:18:56 -0400

    adding my changes to App.tsx and also my component JobFacts.tsx and the interfaces folder with Job.ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import JobFacts from "./components/JobFacts.tsx";
+import styled from "styled-components";
+import { useEffect, useState } from "react";
+import { Job } from "./interfaces/Job.ts";
 
-function App() {
-  const [count, setCount] = useState(0)
+const ParentDiv = styled.div`
+    width: 80vw;
+    margin: auto;
+    border: 5px black solid;
+`;
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+export default function App() {
+    const [data, setData] = useState<Job[]>([]);
+
+    useEffect(() => {
+        async function fetchData(): Promise<void> {
+            const rawData = await fetch("https://jobicy.com/api/v2/remote-jobs?count=20&geo=usa&industry=marketing&tag=seo");
+            const { jobs }: { jobs: Job[] } = await rawData.json();
+            setData(jobs);
+        }
+
+        fetchData()
+            .then(() => console.log("Job data fetched successfully"))
+            .catch((e: Error) => console.log("There was an error: " + e));
+    }, []);
+
+    return (
+        <ParentDiv>
+            <JobFacts data={data} />
+        </ParentDiv>
+    );
 }
 
-export default App
+
