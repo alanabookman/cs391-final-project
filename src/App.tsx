8c846ba1200c5c3275ae83d89da74392c7f24ff9
James <jamesliu@bu.edu>
2025-04-27 07:52:18 -0400

    Added header with working search + expanded initial API search filters

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import JobFacts from "./components/JobFacts.tsx";
 import styled from "styled-components";
 import { useEffect, useState } from "react";
 import { Job } from "./interfaces/Job.ts";
+import Header from "./components/Header.tsx";
 
 const ParentDiv = styled.div`
     width: 80vw;
@@ -11,10 +12,14 @@ const ParentDiv = styled.div`
 
 export default function App() {
     const [data, setData] = useState<Job[]>([]);
+    const [numJobs, setNumJobs] = useState<number>(3);
 
     useEffect(() => {
         async function fetchData(): Promise<void> {
-            const rawData = await fetch("https://jobicy.com/api/v2/remote-jobs?count=20&geo=usa&industry=marketing&tag=seo");
+            // https://jobicy.com/api/v2/remote-jobs?count=${numJobs}&geo=usa&industry=marketing&tag=seo this one only returns 3 jobs total
+            // I removed the extra filters so the search option can display up to 50 jobs now (Let me know if a change is needed)
+            // -James (https://rapidapi.com/jobicy-jobicy-default/api/jobicy for documentation)
+            const rawData = await fetch(`https://jobicy.com/api/v2/remote-jobs?count=${numJobs}`);
             const { jobs }: { jobs: Job[] } = await rawData.json();
             setData(jobs);
         }
@@ -22,10 +27,11 @@ export default function App() {
         fetchData()
             .then(() => console.log("Job data fetched successfully"))
             .catch((e: Error) => console.log("There was an error: " + e));
-    }, []);
+    }, [numJobs]);
 
     return (
         <ParentDiv>
+            <Header numJobs={numJobs} setNumJobs={setNumJobs} />
             <JobFacts data={data} />
         </ParentDiv>
     );
